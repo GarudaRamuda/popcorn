46f898e0fecf2ae3c29837983bbd8c1979748483
garudaramuda <jjek@ucsc.edu>
2022-10-10 12:48:11 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+title = "";
+
+description = `
+`;
+
+characters = [];
+
+options = {
+  theme: "pixel",
+  isPlayingBgm: true,
+  isReplayEnabled: true,
+};
+
+function update() {  
+  if (!ticks) {}
+}
+
+addEventListener("load", onLoad);
