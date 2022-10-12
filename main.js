885edfb15c6ce8ed509e30b5cda306067f27ea55
garudaramuda <jjek@ucsc.edu>
2022-10-12 14:01:42 -0700

    2nd rotation, scroll

--- main.js
+++ main.js
@@ -46,7 +46,9 @@ let flameArray;
 let nextFlameTicks;
 let nextPopTicks;
 let multiplier;
-let rotComplete;
+let rot1Complete;
+let rot2Complete;
+let scr;
 
 function update() {  
   if (!ticks) {
@@ -74,13 +76,15 @@ function update() {
     nextFlameTicks = 30;
     nextPopTicks = 30;
     multiplier = difficulty;
-    rotComplete = false;
+    rot1Complete = false;
+    rot2Complete = false;
+    scr = 0;
 
     for (i = 0; i < 9; i++) {
       kernelArray.push({
-        pos: vec(50 + rnd(-20, 20),50),
+        pos: vec(50 + rnd(-potWidth / 2, potWidth / 2),70 + rnd(-10, 10)),
         pastPos: vec(50, 50),
-        vel: vec(0,0),
+        vel: vec((rnd(-1, 1)),0),
       });
     }
     needsInit = false;
@@ -108,11 +112,13 @@ function update() {
     --nextFlameTicks;
     --nextPopTicks;
 
+    // pop corn
     if (nextPopTicks <= 0) {
       nextPopTicks = rndi(30, 60);
       let pop = kernelArray.pop();
       pop.vel = vec(pop.vel).add(rnd(-4, 4), rnd(-6, -4));
       cookedArray.push(pop);
+      ++cornCooked;
     }
     // we also perform the spawning (not handling!) of all flame particles in this block
     if (nextFlameTicks <= 0) {
@@ -126,20 +132,32 @@ function update() {
   }
   
   if (postCooking) {
-    if (lid.anchor.x > 14) lid.anchor.x -= 0.5;
+    // rotate lid
+    if (lid.anchor.x > 14 && !rot1Complete) lid.anchor.x -= 0.5;
     if (lid.angle > -PI / 2) {
-      lid.angle -= 0.075
+      lid.angle -= 0.075;
     } else {
-      rotComplete = true;
+      rot1Complete = true;
     }
+    if (scr < 50 && rot1Complete) scr += 0.1 + scr / 10;
+    if (rot1Complete) {
+      if (lid.anchor.x < 80) {
+        lid.anchor.x += 1;
+        lid.anchor.x = Math.min(lid.anchor.x, 80);
+      }
+      if (lid.angle > -PI) {
+        lid.angle -= 0.075;
+        lid.angle = Math.max(lid.angle, -PI);
+      }
+    }   
   }
   color("light_cyan");
-  line(pot.pos, vec(pot.pos).add(potWidth, 0));
+  line(vec(pot.pos).add(0, scr), vec(pot.pos).add(potWidth, scr));
   color("cyan");
-  line(pot.pos, vec(pot.pos).add(0, -potHeight));
-  line(vec(pot.pos).add(potWidth, 0), vec(pot.pos).add(potWidth, -potHeight));
+  line(vec(pot.pos).add(0, scr), vec(pot.pos).add(0, -potHeight + scr));
+  line(vec(pot.pos).add(potWidth, scr), vec(pot.pos).add(potWidth, -potHeight + scr));
   lid.pos = vec(lid.anchor).addWithAngle(lid.angle, potWidth / 2);
-  char("a", lid.pos, 
+  char("a", vec(lid.pos).add(0, Math.min(scr, 20)), 
     {
       scale: {x: 12 * Math.cos(lid.angle) - 3 * Math.sin(lid.angle), y: 12 * Math.sin(lid.angle) + 3 * Math.cos(lid.angle)},
       rotation: lid.angle / (PI / 2),
@@ -148,9 +166,8 @@ function update() {
   // this is where we can actually handle moving and drawing the flame particles we spawned
   remove(flameArray, (f) => {
     // adjust the flames position using its velocity
-
     // draw a box at flame's current position
-    box(vec(f.pos), 3);
+    box(vec(f.pos).add(0, scr), 3);
 
     // also have room to mess with the flame's velocity; apply random scalars to simulate
     // air friction or air currents; could also use a sin() or cos() to generate wave motion
@@ -191,16 +208,18 @@ function update() {
       reflect(k, k.pos.x < 50 ? 0 : PI, "cyan");
     }
     color("transparent");
-    while (k.pos.y + k.vel.y > pot.pos.y - kernelDim)
+    while (k.pos.y + k.vel.y > pot.pos.y + scr - kernelDim)
     {
       if (k.vel.y > 0) k.vel.y *= -0.8;
       k.pos.y += -0.1;
     }
+    if (k.pos.y < -10 || k.pos.y > 110) return true;
   })
 
   remove(cookedArray, (k) => {
     k.pastPos.set(k.pos);
-    if (k.vel.y < 0.5 && rnd() > 0.996 && cooking)
+    // randomly fling kernels upwards
+    if (k.vel.y < 0.5 && rnd() > 0.993 && cooking)
     {
       k.vel = vec(k.vel).add(rnd(-2.5, 2.5), rnd(-3, -2));
     }
@@ -228,11 +247,13 @@ function update() {
       reflect(k, k.pos.x < 50 ? 0 : PI, "cyan");
     }
     color("transparent");
-    while (k.pos.y + k.vel.y > pot.pos.y - 3)
+    while (k.pos.y + k.vel.y > pot.pos.y + scr - 3)
     {
       if (k.vel.y > 0) k.vel.y *= -0.8;
       k.pos.y += -0.1;
     }
+
+    if (k.pos.y < -10 || k.pos.y > 110) return true;
   })
 }
 // Original code by Kenta Cho
